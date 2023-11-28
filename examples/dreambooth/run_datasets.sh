df13b95a487b58ccc1efb79c5c0ffe93029fcd87
christopher5106 <christopher.bourez@gmail.com>
2023-11-28 20:39:10 +0100

    n

--- examples/dreambooth/run_datasets.sh
+++ examples/dreambooth/run_datasets.sh
@@ -2,17 +2,17 @@
 HOME=/home/ubuntu
 mkdir -p MODELS
 
-python3 train_dreambooth_lora_sdxl.py \
-  --pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0"  \
-  --instance_data_dir="$HOME/phoenix/data/inputs/tests/train/rayman/inputs/" \
-  --instance_prompt="daiton" \
-  --resolution=1024 \
-  --max_train_steps=3000 \
-  --seed=3407 \
-  --lr_scheduler="constant" \
-  --validation_prompt="daiton" \
-  --output_dir="MODELS/rayman/" \
-  --wandb
+#python3 train_dreambooth_lora_sdxl.py \
+#  --pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0"  \
+#  --instance_data_dir="$HOME/phoenix/data/inputs/tests/train/rayman/inputs/" \
+#  --instance_prompt="daiton" \
+#  --resolution=1024 \
+#  --max_train_steps=3000 \
+#  --seed=3407 \
+#  --lr_scheduler="constant" \
+#  --validation_prompt="daiton" \
+#  --output_dir="MODELS/rayman/" \
+#  --wandb
 
 
 for DATASET in blonde  bubbleverse 'lineart backdrop' 'newrayman running' sword 'vintage photo' ;
