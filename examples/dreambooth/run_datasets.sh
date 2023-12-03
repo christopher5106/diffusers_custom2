2f8f12079fbeec9994894ad3aed34951efb8f10d
christopher5106 <christopher.bourez@gmail.com>
2023-12-03 09:54:48 +0100

    n

--- examples/dreambooth/run_datasets.sh
+++ examples/dreambooth/run_datasets.sh
@@ -15,7 +15,7 @@ VAE_PATH="madebyollin/sdxl-vae-fp16-fix"
 #  --output_dir="MODELS/rayman/" \
 #  --wandb
 
-RANK=4
+RANK=64
 mkdir -p MODELS_$RANK
 #for DATASET in blonde  bubbleverse 'lineart backdrop' 'newrayman running' sword 'vintage photo' rayman3;
 for DATASET in rayman3;
@@ -31,26 +31,25 @@ do
 	echo "Dataset: $HOME/datasets/$DATASET";
   echo $POSTPROMPT;
 
-#  python3 train_dreambooth_lora_sdxl.py \
-#  --instance_data_dir="$HOME/datasets/$DATASET" \
-#  --pretrained_model_name_or_path=$MODEL_NAME  \
-#  --output_dir="MODELS_$RANK/$DATASET/" \
-#  --instance_prompt="daiton$POSTPROMPT"  \
-#  --resolution=1024 \
-#  --rank $RANK \
-#  --train_text_encoder \
-#  --train_batch_size=1 \
-#  --gradient_accumulation_steps=1 \
-#  --learning_rate=1e-4 \
-#  --lr_warmup_steps=0 \
-#  --max_train_steps=3000 \
-#  --seed=3407 \
-#  --lr_scheduler="constant" \
-#  --pretrained_vae_model_name_or_path=$VAE_PATH \
-#  --mixed_precision="fp16" \
-#  --validation_prompt="daiton$POSTPROMPT" \
-#  --report_to="wandb"
-
+  python3 train_dreambooth_lora_sdxl.py \
+  --instance_data_dir="$HOME/datasets/$DATASET" \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --output_dir="MODELS_$RANK/$DATASET/" \
+  --instance_prompt="daiton$POSTPROMPT"  \
+  --resolution=1024 \
+  --rank $RANK \
+  --train_text_encoder \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=1e-4 \
+  --lr_warmup_steps=0 \
+  --max_train_steps=3000 \
+  --seed=3407 \
+  --lr_scheduler="constant" \
+  --pretrained_vae_model_name_or_path=$VAE_PATH \
+  --mixed_precision="fp16" \
+  --validation_prompt="daiton$POSTPROMPT" \
+  --report_to="wandb"
 
   for CHECKPOINT in "checkpoint-1500" "checkpoint-3000"; # checkpoint-5000
   do
