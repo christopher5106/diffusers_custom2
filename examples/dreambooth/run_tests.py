90ac06d1316a94d0e5dc64372ca6b6cb7b93d1bc
christopher5106 <christopher.bourez@gmail.com>
2023-12-07 16:42:51 +0100

    n

--- examples/dreambooth/run_tests.py
+++ examples/dreambooth/run_tests.py
@@ -7,26 +7,26 @@ RANK=4
 DATASETPATH = f"/home/ubuntu/phoenix/data/inputs/tests/train/{DATASET}/inputs/"
 # DATASETPATH = "/home/ubuntu/datasets/$DATASET";
 
-args = parse_args()
-args.instance_data_dir=DATASETPATH
-args.pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0"
-args.output_dir=f"MODELS_{RANK}/{DATASET}"
-args.instance_prompt="daiton"
-args.resolution=1024
-args.rank=RANK
-args.train_text_encoder=True
-args.train_batch_size=1
-args.gradient_accumulation_steps=1
-args.learning_rate=1e-4
-args.lr_warmup_steps=0
-args.max_train_steps=1500
-args.seed=3407
-args.lr_scheduler="constant"
-args.pretrained_vae_model_name_or_path="madebyollin/sdxl-vae-fp16-fix"
-args.mixed_precision="fp16"
-args.validation_prompt="daiton"
-args.report_to="wandb"
-
+args = parse_args([
+    "--instance_data_dir", DATASETPATH,
+    "--pretrained_model_name_or_path", "stabilityai/stable-diffusion-xl-base-1.0",
+    "--output_dir", f"MODELS_{RANK}/{DATASET}",
+    "--instance_prompt", ""daiton",
+    "--resolution", "1024",
+    "--rank", RANK,
+    "--train_text_encoder",
+    "--train_batch_size", "1",
+    "--gradient_accumulation_steps", "1",
+    "--learning_rate", "1e-4",
+    "--lr_warmup_steps", "0",
+    "--max_train_steps", "1500",
+    "--seed", "3407",
+    "--lr_scheduler", "constant",
+    "--pretrained_vae_model_name_or_path", "madebyollin/sdxl-vae-fp16-fix",
+    "--mixed_precision", "fp16",
+    "--validation_prompt", "daiton",
+    "--report_to", "wandb"
+])
 
 print(args.train_text_encoder, args.resolution, args.rank)
 main(args)
