b087efa60dfdd1cefffb448f3e6e76bf54237a85
christopher5106 <christopher.bourez@gmail.com>
2023-12-08 16:20:33 +0100

    n

--- examples/dreambooth/run_tests.py
+++ examples/dreambooth/run_tests.py
@@ -83,9 +83,9 @@ for test in tests:
 
         for replacement in replacements:
             p = (concept_prompt + " " + prompt).replace(to_replace, replacement)
-            html += p
+            html += "<p>" + p + "</p>"
             gridimage_path = f"results/" + (str(prompt[:100]).replace(".", "") + ".png")
-            html += f"<img src={gridimage_path} />"
+            html += f"<img src='{gridimage_path}' /><br/>"
 
     with open(result_dir / f"{dataset}.html", "w") as f:
         f.write(html)
