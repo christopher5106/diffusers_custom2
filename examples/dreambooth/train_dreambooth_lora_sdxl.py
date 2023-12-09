d6a1192d8d57c83b71e4df48b7f269b1de60d4e2
christopher5106 <christopher.bourez@gmail.com>
2023-12-09 17:47:57 +0100

    n

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -69,11 +69,13 @@ from typing import Optional
 class CLIPTextEmbeddingsSpecialToken(nn.Module):
     def __init__(self, CLIPTextEmbeddings):
         super().__init__()
+        self.subnet = CLIPTextEmbeddings
+
         # embed_dim = config.hidden_size
 
-        self.token_embedding = CLIPTextEmbeddings.token_embedding # nn.Embedding(config.vocab_size, embed_dim)
-        self.position_embedding = CLIPTextEmbeddings.position_embedding # nn.Embedding(config.max_position_embeddings, embed_dim)
-        self.position_ids = CLIPTextEmbeddings.position_ids
+        # self.token_embedding = CLIPTextEmbeddings.token_embedding # nn.Embedding(config.vocab_size, embed_dim)
+        # self.position_embedding = CLIPTextEmbeddings.position_embedding # nn.Embedding(config.max_position_embeddings, embed_dim)
+        # self.position_ids = CLIPTextEmbeddings.position_ids
 
         # # position_ids (1, len position emb) is contiguous in memory and exported when serialized
         # self.register_buffer(
@@ -88,16 +90,18 @@ class CLIPTextEmbeddingsSpecialToken(nn.Module):
         position_ids: Optional[torch.LongTensor] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
     ) -> torch.Tensor:
-        seq_length = input_ids.shape[-1] if input_ids is not None else inputs_embeds.shape[-2]
 
-        if position_ids is None:
-            position_ids = self.position_ids[:, :seq_length]
-
-        if inputs_embeds is None:
-            inputs_embeds = self.token_embedding(input_ids)
-
-        position_embeddings = self.position_embedding(position_ids)
-        embeddings = inputs_embeds + position_embeddings
+        embeddings = self.subnet(input_ids, position_ids, inputs_embeds)
+        # seq_length = input_ids.shape[-1] if input_ids is not None else inputs_embeds.shape[-2]
+        #
+        # if position_ids is None:
+        #     position_ids = self.position_ids[:, :seq_length]
+        #
+        # if inputs_embeds is None:
+        #     inputs_embeds = self.token_embedding(input_ids)
+        #
+        # position_embeddings = self.position_embedding(position_ids)
+        # embeddings = inputs_embeds + position_embeddings
 
         return embeddings
 
@@ -974,8 +978,8 @@ def main(args):
     )
 
     if args.train_token:
-        text_encoder_one.text_model.embeddings = CLIPTextEmbeddingsSpecialToken(text_encoder_one.text_model.embeddings).to("cuda")
-        text_encoder_two.text_model.embeddings = CLIPTextEmbeddingsSpecialToken(text_encoder_two.text_model.embeddings).to("cuda")
+        text_encoder_one.text_model.embeddings = CLIPTextEmbeddingsSpecialToken(text_encoder_one.text_model.embeddings)
+        text_encoder_two.text_model.embeddings = CLIPTextEmbeddingsSpecialToken(text_encoder_two.text_model.embeddings)
 
 
     vae_path = (
