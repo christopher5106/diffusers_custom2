ec7e3b59a5ba529c2781a80a65705e775297c2fd
christopher5106 <christopher.bourez@gmail.com>
2023-12-09 17:38:53 +0100

    n

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -69,7 +69,7 @@ from typing import Optional
 class CLIPTextEmbeddingsSpecialToken(nn.Module):
     def __init__(self, CLIPTextEmbeddings):
         super().__init__()
-        embed_dim = config.hidden_size
+        # embed_dim = config.hidden_size
 
         self.token_embedding = CLIPTextEmbeddings.token_embedding # nn.Embedding(config.vocab_size, embed_dim)
         self.position_embedding = CLIPTextEmbeddings.position_embedding # nn.Embedding(config.max_position_embeddings, embed_dim)
