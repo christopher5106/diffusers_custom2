8c9dd646fe09af2bd311d54e5109e5a1f6c0b170
christopher5106 <christopher.bourez@gmail.com>
2023-12-10 17:09:15 +0100

    n

--- examples/dreambooth/predict.py
+++ examples/dreambooth/predict.py
@@ -71,8 +71,9 @@ def generate_lora_sdxl_images(
     state_dict, network_alphas = model.lora_state_dict(lora_path)
 
     del state_dict["text_encoder.special_token_embedding"]
-    for param_tensor in state_dict:
-        print(param_tensor)
+    del state_dict["text_encoder_2.special_token_embedding"]
+    # for param_tensor in state_dict:
+    #     print(param_tensor)
 
     # for param_tensor in model.lora_state_dict(lora_path):
     #     print(param_tensor, "\t", model.lora_state_dict(lora_path)[param_tensor].size())
