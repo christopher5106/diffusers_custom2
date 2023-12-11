f53efc43a63aeb5de262603f14516ff2bc6d1c48
christopher5106 <christopher.bourez@gmail.com>
2023-12-11 12:42:03 +0100

    n

--- examples/dreambooth/run_tests.py
+++ examples/dreambooth/run_tests.py
@@ -64,7 +64,7 @@ for test in tests:
             print(f"Train error: {e}")
             traceback.print_exc()
 
-        for checkpoint in ["checkpoint-500","checkpoint-1000", "checkpoint-1500"]:
+        for checkpoint in ["checkpoint-500", "checkpoint-1000", "checkpoint-1500"]:
             lora_path = f"MODELS_{rank}/{dataset}/{replacement}/{checkpoint}/pytorch_lora_weights.safetensors"
             _validation_prompts = [
                 (concept_prompt + " " + p).lower().replace(to_replace, replacement)
