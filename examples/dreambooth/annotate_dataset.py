d9afc2be19658f393c065e207904e8812b2b7920
christopher5106 <christopher.bourez@gmail.com>
2023-12-22 10:55:29 +0100

    n

---
+++ examples/dreambooth/annotate_dataset.py
@@ -0,0 +1,35 @@
+from PIL import Image
+from pathlib import Path
+
+# from transformers import BlipProcessor, BlipForConditionalGeneration
+# processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+# model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to("cuda")
+
+import torch
+from lavis.models import load_model_and_preprocess
+
+device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+model, vis_processors, _ = load_model_and_preprocess(
+    name="blip2_t5",
+    model_type="pretrain_flant5xxl",
+    is_eval=True,
+    device=device)
+
+datasets = Path("/home/ubuntu/datasets/")
+
+for f in [datasets / "golem"]:  # datasets.iterdir():
+    if f.is_dir():
+        for image_path in f.iterdir():
+            print(image_path)
+            if str(image_path)[-4:] == ".png":
+                raw_image = Image.open(image_path).convert('RGB')
+                image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+
+                # inputs = processor(raw_image, return_tensors="pt").to("cuda")
+                # out = model.generate(**inputs)
+                # caption = processor.decode(out[0], skip_special_tokens=True)
+                caption = model.generate({"image": image})
+                # caption = processor.batch_decode(out, skip_special_tokens=True)[0].strip()
+                print("   ", caption)
+                with open(str(image_path)+".txt", "w") as _f:
+                    _f.write(caption)
--- examples/dreambooth/run_tests.py
+++ examples/dreambooth/run_tests.py
@@ -12,7 +12,7 @@ with open("tests.json", "r") as f:
     tests = json.load(f)
 
 rank = 64
-num_steps = 5000
+num_steps = 1500
 num_special_tokens = 1
 
 result_dir = Path("results")
@@ -52,8 +52,8 @@ for test in tests:
         "--gradient_accumulation_steps", "1",
         "--learning_rate", "1e-4",
         "--num_special_tokens", str(num_special_tokens),
-        "--text_specialtoken_lr", "1e-4",
-        "--text_encoder_lr", "0.1",
+        "--text_specialtoken_lr", "1e-3",
+        "--text_encoder_lr", "1e-6",
         "--lr_warmup_steps", "0",
         "--max_train_steps", f"{num_steps}",
         "--seed", "3407",
@@ -72,7 +72,7 @@ for test in tests:
         print(f"Train error: {e}")
         traceback.print_exc()
 
-    for checkpoint in ["checkpoint-1500", "checkpoint-3000", ""]:  # "checkpoint-500",
+    for checkpoint in ["checkpoint-500", "checkpoint-1000", ""]:  # "checkpoint-500",
         lora_path = f"MODELS_{rank}/{dataset}/{replacement}/{checkpoint}/pytorch_lora_weights.safetensors"
         _validation_prompts = [
             (concept_prompt + " " + p).lower().replace(to_replace, replacement)
